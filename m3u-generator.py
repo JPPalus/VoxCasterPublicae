c55dda512f41e729c7965e1fa40817f92806830f
JPPalus <oscmalekith@gmail.com>
2023-03-01 12:36:55 +0100

    cleanupo

---
+++ m3u-generator.py
@@ -0,0 +1,18 @@
+import glob
+from natsort import natsorted
+
+VALID_FILES = ['.mp3', '.wav', '.flac', '.mid', '.ogg']
+SITE_ROOT = 'https://vox-caster.fr/'
+
+files = natsorted(glob.glob('**/*',recursive=True))
+
+playlist = ''
+
+for file in files:
+    for authorized_filetype in VALID_FILES:
+        if file.endswith(authorized_filetype) and file != "playlist.m3u":
+            playlist += SITE_ROOT + file + '\n'
+
+f = open("playlist.m3u", "w")
+f.write(playlist)
+f.close()
